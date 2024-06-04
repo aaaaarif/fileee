7a2ec7a685430ab3573e29fa7b768e7f0d1d907d
aaaaarif <salmanarif717@gmail.com>
2024-06-04 10:23:24 +0600

    Add files via upload

---
+++ Project.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html >
+<head>
+    
+</head>
+<body>
+    <h1>hello world</h1>
+
+
+    <?php
+    $a 5;
+    $b 3;
+    echo"The summation";$a+$b;
+    echo "br/<";
+    echo 
+    ?>
+</body>
+</html>
